c08924ac523aacc8898fdb8f5e84c4787c2f7aa8
Andre Cronje <andre@fantom.foundation>
2020-01-17 02:45:31 +0200

    Compiler issues
    Signed-off-by: Andre Cronje <andre@fantom.foundation>

--- contracts/liquiditypool/LiquidityPool.sol
+++ contracts/liquiditypool/LiquidityPool.sol
@@ -11,21 +11,24 @@ contract LiquidityPool is ReentrancyGuard {
     using Address for address;
 
     // Collateral data
-    mapping(address => mapping(address => uint)) public _collateral;
-    mapping(address => mapping(address => uint)) public _collateralTokens;
+    mapping(address => mapping(address => uint256)) public _collateral;
+    mapping(address => mapping(address => uint256)) public _collateralTokens;
     mapping(address => address[]) public _collateralList;
     //measured in ETH - slippage
-    mapping(address => uint) public _collateralValue;
+    mapping(address => uint256) public _collateralValue;
 
     // Debt data
-    mapping(address => mapping(address => uint)) public _debt;
-    mapping(address => mapping(address => uint)) public _debtTokens;
+    mapping(address => mapping(address => uint256)) public _debt;
+    mapping(address => mapping(address => uint256)) public _debtTokens;
     mapping(address => address[]) public _debtList;
     //measured in ETH + slippage
-    mapping(address => uint) public _debtValue;
+    mapping(address => uint256) public _debtValue;
 
-    function ethAddress() internal pure returns(address) {
-        return 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    function fAddress() internal pure returns(address) {
+        return 0xFffffFfffFfFffFfFfFffFFFffffFfffffffFFfF;
+    }
+    function sAddress() internal pure returns(address) {
+        return 0xFffffFfffFfFffFfFfFffFFFffffFfffffffFFfF;
     }
 
     function addCollateralToList(address _token, address _owner) internal {
@@ -76,6 +79,28 @@ contract LiquidityPool is ReentrancyGuard {
         uint256 _timestamp
     );
 
+    function calcCollateralValue(address addr) public view returns(uint256 collateralValue)
+    {
+        uint256 collateralValue = 0;
+        uint256 results = 0;
+        for (uint i = 0; i < _collateralList[addr].length; i++) {
+          (results,) = OneSplit(sAddress()).getExpectedReturn(IERC20(_collateralList[addr][i]), IERC20(fAddress()), _collateralTokens[addr][_collateralList[addr][i]], 1, 0);
+          collateralValue = collateralValue.add(results);
+        }
+        return collateralValue;
+    }
+
+    function calcDebtValue(address addr) public view returns(uint256 collateralValue)
+    {
+      uint256 debtValue = 0;
+      uint256 results = 0;
+      for (uint i = 0; i < _debtList[msg.sender]; i++) {
+        (results,) = OneSplit(sAddress()).getExpectedReturn(IERC20(_debtList[addr][i]), IERC20(fAddress()), _debtTokens[addr][_debtList[addr][i]], 1, 0);
+        debtValue = debtValue.add(results);
+      }
+      return debtValue;
+    }
+
     function deposit(address _token, uint256 _amount)
         external
         payable
@@ -83,21 +108,14 @@ contract LiquidityPool is ReentrancyGuard {
     {
         require(_amount > 0, "amount must be greater than 0");
 
-
         _collateral[_token][msg.sender] = _collateral[_token][msg.sender].add(_amount);
         _collateralTokens[msg.sender][_token] = _collateralTokens[msg.sender][_token].add(_amount);
         addCollateralToList(_token, msg.sender);
-        uint256 collateralValue = 0;
-        uint256[] memory results;
-        for (uint i = 0; i < _collateralList[msg.sender].length; i++) {
-          (results) = OneSplit.getAllRatesForDEX(_collateralList[msg.sender][i], ethAddress(), _collateralTokens[msg.sender][_collateralList[msg.sender][i]], 1, 0);
-          collateralValue = collateralValue.add(results[0]);
-        }
-        _collateralValue[msg.sender] = collateralValue;
+        _collateralValue[msg.sender] = calcCollateralValue(msg.sender)
 
-        if (_token != ethAddress()) {
+        if (_token != fAddress()) {
             require(msg.value == 0, "user is sending ETH along with the ERC20 transfer.");
-            IERC20(_token).safeTransferFrom(msg.sender, address(this), _amount);
+            IERC20(_token).transferFrom(msg.sender, address(this), _amount);
         } else {
             require(msg.value >= _amount, "the amount and the value sent to deposit do not match");
             if (msg.value > _amount) {
@@ -106,7 +124,6 @@ contract LiquidityPool is ReentrancyGuard {
                 require(result, "transfer of ETH failed");
             }
         }
-
         emit Deposit(_token, msg.sender, _amount, block.timestamp);
     }
 
@@ -117,16 +134,14 @@ contract LiquidityPool is ReentrancyGuard {
         require(_amount > 0, "amount must be greater than 0");
         _collateral[_token][msg.sender] = _collateral[_token][msg.sender].sub(_amount, "withdraw amount exceeds balance");
         _collateralTokens[msg.sender][_token] = _collateralTokens[msg.sender][_token].sub(_amount, "withdraw amount exceeds balance");
-        uint256 collateralValue = 0;
-        uint256[] memory results;
-        for (uint i = 0; i < _collateralList[msg.sender]; i++) {
-          (results) = OneSplit.getAllRatesForDEX(_collateralList[msg.sender][i], ethAddress(), _collateralTokens[msg.sender][_collateralList[msg.sender][i]], 1, 0);
-          collateralValue = collateralValue.add(results[0]);
-        }
-        require(collateralValue > _debtValue, "withdraw would liquidate holdings");
+        uint256 collateralValue = calcCollateralValue(msg.sender)
+        uint256 debtValue = calcDebtValue(msg.sender)
+        require(collateralValue > debtValue, "withdraw would liquidate holdings");
         _collateralValue[msg.sender] = collateralValue;
-        if (_token != ethAddress()) {
-            IERC20(_token).safeTransfer(msg.sender, _amount);
+        _debtValue[msg.sender] = debtValue;
+
+        if (_token != fAddress()) {
+            IERC20(_token).transfer(msg.sender, _amount);
         } else {
             (bool result, ) = msg.sender.call.value(_amount).gas(50000)("");
             require(result, "transfer of ETH failed");
@@ -145,30 +160,20 @@ contract LiquidityPool is ReentrancyGuard {
         _debtTokens[msg.sender][_token] = _debtTokens[msg.sender][_token].add(_amount);
         addDebtToList(_token, msg.sender);
 
-        uint256[] memory tokenValue;
-
-        (tokenValue) = OneSplit.getAllRatesForDEX(_token, ethAddress(), _debtTokens[msg.sender][_token], 1, 0);
+        uint256 tokenValue;
+        (tokenValue) = OneSplit(sAddress()).getExpectedReturn(IERC20(_token), IERC20(fAddress()), _debtTokens[msg.sender][_token], 1, 0);
         require(tokenValue[0] > 0, "debt token has no value");
 
-        uint256 collateralValue = 0;
-        uint256[] memory results;
-        for (uint i = 0; i < _collateralList[msg.sender]; i++) {
-          (results) = OneSplit.getAllRatesForDEX(_collateralList[msg.sender][i], ethAddress(), _collateralTokens[msg.sender][_collateralList[msg.sender][i]], 1, 0);
-          collateralValue = collateralValue.add(results[0]);
-        }
-
-        uint256 debtValue = 0;
-        for (uint i = 0; i < _debtList[msg.sender]; i++) {
-          (results) = OneSplit.getAllRatesForDEX(_debtList[msg.sender][i], ethAddress(), _debtTokens[msg.sender][_debtList[msg.sender][i]], 1, 0);
-          debtValue = debtValue.add(results[0]);
-        }
+        uint256 collateralValue = calcCollateralValue(msg.sender)
+        uint256 debtValue = calcDebtValue(msg.sender)
 
         require(collateralValue > debtValue, "insufficient collateral");
+
         _collateralValue[msg.sender] = collateralValue;
         _debtValue[msg.sender] = debtValue;
 
-        if (_token != ethAddress()) {
-            IERC20(_token).safeTransfer(msg.sender, _amount);
+        if (_token != fAddress()) {
+            IERC20(_token).transfer(msg.sender, _amount);
         } else {
             (bool result, ) = msg.sender.call.value(_amount).gas(50000)("");
             require(result, "transfer of ETH failed");
@@ -187,26 +192,12 @@ contract LiquidityPool is ReentrancyGuard {
         _debt[_token][msg.sender] = _debt[_token][msg.sender].sub(_amount, "insufficient debt outstanding");
         _debtTokens[msg.sender][_token] = _debtTokens[msg.sender][_token].sub(_amount, "insufficient debt outstanding");
 
-        uint256 collateralValue = 0;
-        uint256[] memory results;
-        for (uint i = 0; i < _collateralList[msg.sender]; i++) {
-          (results) = OneSplit.getAllRatesForDEX(_collateralList[msg.sender][i], ethAddress(), _collateralTokens[msg.sender][_collateralList[msg.sender][i]], 1, 0);
-          collateralValue = collateralValue.add(results[0]);
-        }
+        _collateralValue[msg.sender] = calcCollateralValue(msg.sender);
+        _debtValue[msg.sender] = calcDebtValue(msg.sender);
 
-        uint256 debtValue = 0;
-        for (uint i = 0; i < _debtList[msg.sender]; i++) {
-          (results) = OneSplit.getAllRatesForDEX(_debtList[msg.sender][i], ethAddress(), _debtTokens[msg.sender][_debtList[msg.sender][i]], 1, 0);
-          debtValue = debtValue.add(results[0]);
-        }
-
-        require(collateralValue > debtValue, "insufficient collateral");
-        _collateralValue[msg.sender] = collateralValue;
-        _debtValue[msg.sender] = debtValue;
-
-        if (_token != ethAddress()) {
+        if (_token != fAddress()) {
             require(msg.value == 0, "user is sending ETH along with the ERC20 transfer.");
-            IERC20(_token).safeTransferFrom(msg.sender, address(this), _amount);
+            IERC20(_token).transferFrom(msg.sender, address(this), _amount);
         } else {
             require(msg.value >= _amount, "the amount and the value sent to deposit do not match");
             if (msg.value > _amount) {
@@ -223,41 +214,29 @@ contract LiquidityPool is ReentrancyGuard {
         external
         nonReentrant
     {
-        uint256 collateralValue = 0;
-        uint256[] memory results;
-        for (uint i = 0; i < _collateralList[_owner]; i++) {
-          (results) = OneSplit.getAllRatesForDEX(_collateralList[_owner][i], ethAddress(), _collateralTokens[_owner][_collateralList[_owner][i]], 1, 0);
-          collateralValue = collateralValue.add(results[0]);
-        }
+        _collateralValue[_owner] = calcCollateralValue(_owner);
+        _debtValue[_owner] = calcDebtValue(_owner);
 
-        uint256 debtValue = 0;
-        for (uint i = 0; i < _debtList[_owner]; i++) {
-          (results) = OneSplit.getAllRatesForDEX(_debtList[_owner][i], ethAddress(), _debtTokens[_owner][_debtList[_owner][i]], 1, 0);
-          debtValue = debtValue.add(results[0]);
-        }
-
-        _collateralValue[_owner] = collateralValue;
-        _debtValue[_owner] = debtValue;
-
-        require(collateralValue < debtValue, "insufficient debt to liquidate");
+        require(_collateralValue[_owner] < _debtValue[_owner], "insufficient debt to liquidate");
 
         uint256 sold = 0;
-        collateralValue = 0;
+        uint256 collateralValue = 0;
+        uint256 results = 0;
         for (uint i = 0; i < _collateralList[_owner]; i++) {
-          (results) = OneSplit.getAllRatesForDEX(_collateralList[_owner][i], ethAddress(), _collateralTokens[_owner][_collateralList[_owner][i]], 1, 0);
-          OneSplit.goodSwap(_collateralList[_owner][i], ethAddress(), _collateralTokens[_owner][_collateralList[_owner][i]], results[0], 1, 0);
+          (results,) = OneSplit(sAddress()).getExpectedReturn(IERC20(_collateralList[_owner][i]), IERC20(fAddress()), _collateralTokens[_owner][_collateralList[_owner][i]], 1, 0);
+          OneSplit(sAddress()).goodSwap(IERC20(_collateralList[_owner][i]), IERC20(fAddress()), _collateralTokens[_owner][_collateralList[_owner][i]], results, 1, 0);
           _collateral[_collateralList[_owner][i]][_owner] = _collateral[_collateralList[_owner][i]][_owner].sub(_collateralTokens[_owner][_collateralList[_owner][i]], "liquidation exceeds balance");
           _collateralTokens[_owner][_collateralList[_owner][i]] = _collateralTokens[_owner][_collateralList[_owner][i]].sub(_collateralTokens[_owner][_collateralList[_owner][i]], "liquidation exceeds balance");
-          sold = sold.add(results[0]);
-          collateralValue = collateralValue.add(results[0]);
+          sold = sold.add(results);
+          collateralValue = collateralValue.add(results);
         }
 
         debtValue = 0;
         for (uint i = 0; i < _debtList[_owner]; i++) {
-          (results) = OneSplit.getAllRatesForDEX(_debtList[_owner][i], ethAddress(), _debtTokens[_owner][_debtList[_owner][i]], 1, 0);
-          sold = sold.sub(results[0]);
+          (results,) = OneSplit(sAddress()).getExpectedReturn(IERC20(_debtList[_owner][i]), IERC20(fAddress()), _debtTokens[_owner][_debtList[_owner][i]], 1, 0);
+          sold = sold.sub(results);
           if (sold >= 0) {
-            OneSplit.goodSwap(ethAddress(), _debtList[_owner][i], _debtTokens[_owner][_collateralList[_owner][i]], results[0], 1, 0);
+            OneSplit(sAddress()).goodSwap(IERC20(fAddress()), IERC20(_debtList[_owner][i]), _debtTokens[_owner][_collateralList[_owner][i]], results[0], 1, 0);
             _debt[_collateralList[_owner][i]][_owner] = _debt[_collateralList[_owner][i]][_owner].sub(_debt[_owner][_collateralList[_owner][i]], "liquidation exceeds balance");
             _debtTokens[_owner][_collateralList[_owner][i]] = _debtTokens[_owner][_collateralList[_owner][i]].sub(_debtTokens[_owner][_collateralList[_owner][i]], "liquidation exceeds balance");
             debtValue = debtValue.add(results[0]);
@@ -274,13 +253,13 @@ contract LiquidityPool is ReentrancyGuard {
         uint256 collateralValue = 0;
         uint256[] memory results;
         for (uint i = 0; i < _collateralList[_owner]; i++) {
-          (results) = OneSplit.getAllRatesForDEX(_collateralList[_owner][i], ethAddress(), _collateralTokens[_owner][_token], 1, 0);
+          (results) = OneSplit.getExpectedReturn(_collateralList[_owner][i], fAddress(), _collateralTokens[_owner][_token], 1, 0);
           collateralValue = collateralValue.add(results[0]);
         }
 
         uint256 debtValue = 0;
         for (uint i = 0; i < _debtList[_owner]; i++) {
-          (results) = OneSplit.getAllRatesForDEX(_debtList[_owner][i], ethAddress(), _debtTokens[_owner][_token], 1, 0);
+          (results) = OneSplit.getExpectedReturn(_debtList[_owner][i], fAddress(), _debtTokens[_owner][_token], 1, 0);
           debtValue = debtValue.add(results[0]);
         }
 
@@ -290,16 +269,16 @@ contract LiquidityPool is ReentrancyGuard {
         require(collateralValue < debtValue, "insufficient debt to liquidate");
 
         uint256 sold = 0;
-        (results) = OneSplit.getAllRatesForDEX(_token, ethAddress(), _collateralTokens[_owner][_token], 1, 0);
-        OneSplit.goodSwap(_token, ethAddress(), _collateralTokens[_owner][_token], results[0], 1, 0);
+        (results) = OneSplit.getExpectedReturn(_token, fAddress(), _collateralTokens[_owner][_token], 1, 0);
+        OneSplit.goodSwap(_token, fAddress(), _collateralTokens[_owner][_token], results[0], 1, 0);
         _collateral[_token][_owner] = _collateral[_token][_owner].sub(_collateralTokens[_owner][_token], "liquidation exceeds balance");
         _collateralTokens[_owner][_token] = _collateralTokens[_owner][_token].sub(_collateralTokens[_owner][_token], "liquidation exceeds balance");
         sold = sold.add(results[0]);
 
-        (results) = OneSplit.getAllRatesForDEX(_token, ethAddress(), _debtTokens[_owner][_token], 1, 0);
+        (results) = OneSplit.getExpectedReturn(_token, fAddress(), _debtTokens[_owner][_token], 1, 0);
         sold = sold.sub(results[0]);
         if (sold >= 0) {
-          OneSplit.goodSwap(ethAddress(), _token, _debtTokens[_owner][_token], results[0], 1, 0);
+          OneSplit.goodSwap(fAddress(), _token, _debtTokens[_owner][_token], results[0], 1, 0);
           _debt[_token][_owner] = _debt[_token][_owner].sub(_debt[_owner][_token], "liquidation exceeds balance");
           _debtTokens[_owner][_token] = _debtTokens[_owner][_token].sub(_debtTokens[_owner][_token], "liquidation exceeds balance");
         }
